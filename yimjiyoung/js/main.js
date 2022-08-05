324567cd91d0ad7379a11eb40417cebaae1b3da8
Roxy100 <misoamy16@gmail.com>
2022-08-05 15:13:46 +0900

    Add: 미션4 추가중

--- yimjiyoung/js/main.js
+++ yimjiyoung/js/main.js
@@ -1,26 +1,36 @@
-const writeInput = document.getElementsByClassName('write-comment-input');
+const writeInput = document.getElementsByClassName('write-comment-input'); // 스프레드 문법
 const writeBtn = document.getElementsByClassName('write-comment-btn');
-const userComments = document.getElementsByClassName('feeds-comments');
+const userComments = document.getElementsByClassName('feeds-comments'); //HTMLCollection << Array-like object 동적 객체
+const USERID = 'roxylife'; // 댓글 아이디
 
 // writeBtn.disabled = true;
 
 // 댓글 입력 함수 작성
 Array.from(writeBtn).forEach((button, index) => {
-  button.addEventListener('click', () => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
     const content = writeInput[index].value;
     const childComment = document.createElement('div');
-    const childNickname = document.createElement('span');
+    childComment.className = 'user-comment';
+    childComment.innerHTML = `<span class="nickname">${USERID}</span> <span>${content}</span>`;
 
-    childNickname.className = 'nickname';
-    childNickname.textContent = 'roxylife';
+    userComments[index].appendChild(childComment);
+    writeInput[index].value = '';
+  });
+});
 
-    childComment.prepend(childNickname, content);
-
-    userComments[index].prepend(childComment);
-    writeInput = '';
-    writeBtn.style.color = 'blue';
-    writeBtn.disabled = true;
-    writeBtn.style.cursor = 'auto';
+Array.from(writeInput).forEach((el, index) => {
+  el.addEventListener('input', () => {
+    if (writeInput[index].value) {
+      writeBtn[index].style.color = 'blue';
+      writeBtn[index].removeAttribute('disabled');
+      console.log('value here');
+    } else {
+      writeBtn[index].style.color = '#6cc1d9;'; // Error
+      writeBtn[index].disabled = true;
+      console.log('empty');
+    }
+    // writeBtn[index].style.cursor = 'auto';
   });
 });
 
